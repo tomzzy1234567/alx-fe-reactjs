f13c4ffa77f249cc28368f90420e09607c7f3a85
Tomiwa odunukan <150262149+tomzzy1234567@users.noreply.github.com>
2025-02-12 11:26:24 +0100

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -9,3 +9,19 @@ function App() {
 }
 
 export default App;
+
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
